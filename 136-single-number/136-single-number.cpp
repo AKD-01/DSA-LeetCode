62d1b8dde86df5b14088b3355650b1852e8a3aec
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-08-06 14:05:58 +0530

    Time: 47 ms (20.81%), Space: 20.2 MB (12.40%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,11 +3,11 @@ public:
     int singleNumber(vector<int>& nums) {
         unordered_map<int,int> m;
         int ans=0;
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+        for(auto it:nums) {
+            m[it]++;
         }
-        for(int i=0;i<nums.size();i++){
-            if(m[nums[i]]==1) ans=nums[i];
+        for(auto it:nums){
+            if(m[it]==1) ans=it;
         }
         return ans;
     }
