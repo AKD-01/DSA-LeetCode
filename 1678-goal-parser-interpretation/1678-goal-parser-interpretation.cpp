7ec4a143e262e23955e426a297c42d93e7a00aef
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-08-29 20:41:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (57.23%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    string interpret(string s) {
+    string interpret(string command) {
         string ans;
-        for (int i = 0; i < s.size(); ) {
-            if (s[i] == 'G') ans += 'G', ++i;
-            else if (s[i + 1] == ')') ans += 'o', i += 2;
+        for (int i = 0; i < command.size(); ) {
+            if (command[i] == 'G') ans += 'G', ++i;
+            else if (command[i + 1] == ')') ans += 'o', i += 2;
             else ans += "al", i += 4;
         }
         return ans;
