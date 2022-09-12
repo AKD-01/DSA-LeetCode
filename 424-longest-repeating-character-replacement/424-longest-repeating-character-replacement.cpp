4826ee7d0162586d64345e4e66b1191304c8ec2c
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-09-12 09:58:59 +0530

    Time: 18 ms (53.42%), Space: 7.1 MB (51.55%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+       int res=0, maxf=0;
+       unordered_map<int, int> count;
+        for (int i=0; i<s.length(); ++i) {
+            maxf = max(maxf, ++count[s[i]]);
+            if (res-maxf<k) res++;
+            else count[s[i - res]]--;
+        }
+        return res; 
+    }
+};
