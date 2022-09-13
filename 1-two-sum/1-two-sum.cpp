a4ac5a05712aac7ce867a9a181763e489b42a540
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-09-13 12:11:32 +0530

    Time: 27 ms (52.99%), Space: 10.9 MB (46.88%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       int n=nums.size();
-       vector<pair<int,int>> v;
-       for(int i=0;i<n;i++) v.push_back({nums[i],i});
-       sort(v.begin(),v.end());
-       int s=0, e=n-1;
-       while(s<e) {
-           int sum=v[s].first+v[e].first;
-           if(sum==target) return {v[s].second,v[e].second}; 
-           else if (sum>target) e--;
-           else s++;
-       }
-        return {-1,-1};
+      unordered_set<int> s;
+      vector<int> result;
+      for(int i=0;i<nums.size();i++){
+          int x=target-nums[i];
+          if(s.find(x)==s.end()) {
+              s.insert(nums[i]);
+          }
+          else {
+              result.push_back(i);
+              auto it=find(nums.begin(), nums.end(), x);
+              int index=it-nums.begin();
+              result.push_back(index);
+              return result;
+          }
+      }
+        return result;
     }
 };
 
@@ -31,5 +35,18 @@ unordered_map<int,int> m;
             if(m.find(y)!=m.end()) return {i,m[y]};
             m[nums[i]]=i;
         }
+        return {-1,-1};
+        
+vector<pair<int,int>> v;
+       int n=nums.size();
+       for(int i=0;i<n;i++) v.push_back({nums[i],i});
+       sort(v.begin(),v.end());
+       int s=0, e=n-1;
+       while(s<e) {
+           int sum=v[s].first+v[e].first;
+           if(sum==target) return {v[s].second,v[e].second}; 
+           else if (sum>target) e--;
+           else s++;
+       }
         return {-1,-1};
 */
