144ccc04d0d775d9098f1c104acade1d8b064d38
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-09-13 09:11:05 +0530

    Time: 27 ms (52.99%), Space: 11 MB (23.30%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        for(int i=0;i<nums.size();i++) {
-            int x=nums[i];
-            int y=target-x;
-            if(m.find(y)!=m.end()) return {i,m[y]};
-            m[nums[i]]=i;
-        }
+       int n=nums.size();
+       vector<pair<int,int>> v;
+       for(int i=0;i<n;i++) v.push_back({nums[i],i});
+       sort(v.begin(),v.end());
+       int s=0, e=n-1;
+       while(s<e) {
+           int sum=v[s].first+v[e].first;
+           if(sum==target) return {v[s].second,v[e].second}; 
+           else if (sum>target) e--;
+           else s++;
+       }
         return {-1,-1};
     }
 };
@@ -17,6 +21,15 @@ for(int i=0;i<nums.size();i++) {
             for(int j=i+1;j<nums.size();j++) {
                 if(nums[i]+nums[j]==target) return {i,j};
             }
+        }
+        return {-1,-1};
+        
+unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++) {
+            int x=nums[i];
+            int y=target-x;
+            if(m.find(y)!=m.end()) return {i,m[y]};
+            m[nums[i]]=i;
         }
         return {-1,-1};
 */
