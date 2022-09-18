b378f4af5b61188c643d93a06fa22e5882b486ff
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-09-18 22:59:19 +0530

    Time: 1043 ms (5.04%), Space: 613.7 MB (5.00%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,7 +1,41 @@
 class Solution {
 public:
+    bool OutofOrder(vector<int> nums, int i) {
+        int x=nums[i];
+        if(i==0) {
+            return x>nums[1];
+        }
+        if(i==nums.size()-1) {
+            return x<nums[i-1];
+        }
+        return x>nums[i+1] or x<nums[i-1];
+    }
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> copy(nums);
+        if(nums.size()==0 || nums.size()==1) return 0;
+        int smallest=INT_MAX;
+        int largest=INT_MIN;
+        for(int i=0;i<nums.size();i++) {
+            int x=nums[i];
+            if(OutofOrder(nums,i)) {
+                smallest=min(smallest,x);
+                largest=max(largest,x);
+            }
+        }
+        if(smallest==INT_MAX) return 0;
+        int left=0;
+        while(smallest>=nums[left]) {
+            left++;
+        }
+        int right=nums.size()-1;
+        while(largest<=nums[right]) {
+            right--;
+        }
+       return right-left+1;
+    }
+};
+
+/* 
+ vector<int> copy(nums);
         int n=nums.size();
         sort(nums.begin(),nums.end());
         int l=0;
@@ -22,5 +56,4 @@ public:
             }
         }
         return count;
-    }
-};
+*/
