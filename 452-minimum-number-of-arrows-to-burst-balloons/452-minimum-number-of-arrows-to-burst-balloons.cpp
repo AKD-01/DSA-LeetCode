75cfa21cb85cf29224804d0b587e7565a300ebd0
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-09-25 17:04:13 +0530

    Time: 657 ms (64.61%), Space: 102.4 MB (7.44%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,22 +1,21 @@
 class Solution {
-static bool cmp(vector<int>& point_1, vector<int>& point_2) {
-	if(point_1[1] == point_2[1])
-		return point_1[0] < point_2[0];
-	return point_1[1] < point_2[1];
-}
 public:
+    static bool cmp(vector<int> &point_1, vector<int> &point_2) {
+        if(point_1[1]==point_2[1]) {
+            return point_1[0]<point_2[0];
+        }
+        return point_1[1]<point_2[1];
+    }
     int findMinArrowShots(vector<vector<int>>& points) {
-
-	sort(points.begin(), points.end(), cmp); // sort points, with points having small endpoint comes first
-
-	int last_point_where_we_shot_an_arrow = points[0][1]; // we shoot an arrow at the smallest endpoint.
-	int shots_fired = 1;
-	for(auto& point : points) {
-		if(point[0] > last_point_where_we_shot_an_arrow) { // if this segment doesn't pass through the last point where we shot an arrow, pick this segment and shoot an arrow at its endpoint
-			shots_fired++;
-			last_point_where_we_shot_an_arrow = point[1];
-		}
-	}
-	return shots_fired;
+        sort(points.begin(), points.end(), cmp);
+        int lastpointshot=points[0][1];
+        int shotfired=1;
+        for(auto point:points) {
+            if(point[0]>lastpointshot) {
+                shotfired++;
+                lastpointshot=point[1];
+            }
+        }
+        return shotfired;
     }
 };
