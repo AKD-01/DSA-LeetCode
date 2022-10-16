d3063180f1c9760c63a5474fb8e8550e2f6c2f1f
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-10-16 19:15:58 +0530

    Time: 306 ms (46.11%), Space: 57.2 MB (49.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,48 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-      mergesort(nums,0,nums.size()-1);
-      return nums;
-    }
-    void mergesort(vector<int> &nums,int s, int e) {
-        //basecase
-        if(s>=e) {
-            return;
-        }
-        //recursive call
-        int mid=(s+e)/2;
-        mergesort(nums,s,mid);
-        mergesort(nums,mid+1,e);
-        return merge(nums,s,e);
-    }
-    void merge(vector<int> &nums, int s, int e){
-        int i=s;
-        int m=(s+e)/2;
-        int j=m+1;
-        vector<int> temp;
-        while(i<=m && j<=e) {
-            if(nums[i]<nums[j]) {
-                temp.push_back(nums[i]);
-                i++;
-            }
-            else {
-                temp.push_back(nums[j]);
-                j++;
-            }
-        }
-        //copy remaining elements from first array
-        while(i<=m) {
-            temp.push_back(nums[i++]);
-        }
-        //copy remaining elements from second array
-        while(j<=e) {
-            temp.push_back(nums[j++]);
-        }
-        //copy back elements from temp to original array 
-        int k=0;
-        for(int i=s;i<=e;i++) {
-            nums[i]=temp[k++];
-        }
-        return;
+        sort(nums.begin(),nums.end());  
+        return nums;
     }
 };
