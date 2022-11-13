07043136782032d197990e7ef7ebff27904771a5
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-11-13 08:48:48 +0530

    Time: 4 ms (91.32%), Space: 7.1 MB (83.99%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        reverse(s.begin(), s.end());
+        int l = 0, r = 0, i = 0, n = s.size();
+        while (i < n) {
+            while (i < n && s[i] != ' ')
+                s[r++] = s[i++];
+
+            if (l < r) { // if we can find a non-empty word then
+                reverse(s.begin() + l, s.begin() + r); // reverse current word
+                if (r == n) break;
+                s[r++] = ' '; // set empty space
+                l = r;
+            }
+            ++i; // now i == n or s[i] == ' ', so we skip that character!
+        }
+        if (r > 0 && s[r-1] == ' ') --r; // skip last empty character if have
+        s.resize(r);
+        return s;
+    }
+};
