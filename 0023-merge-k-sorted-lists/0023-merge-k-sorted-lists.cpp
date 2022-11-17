e1a798b138f41edc34f34a14d15a3372c344079f
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2022-11-17 20:07:13 +0530

    Time: 513 ms (11.63%), Space: 13.5 MB (47.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -14,7 +14,7 @@ public:
          if(lists.empty()){
         return nullptr;
     }
-    while(lists.size() > 1){
+    while(lists.size() > 1) {
         lists.push_back(mergeTwoLists(lists[0], lists[1]));
         lists.erase(lists.begin());
         lists.erase(lists.begin());
@@ -22,19 +22,19 @@ public:
     return lists.front();
     }
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-    if(l1 == nullptr){
+        if(l1 == nullptr){
         return l2;
-    }
-    if(l2 == nullptr){
+        }
+        if(l2 == nullptr){
         return l1;
-    }
-    if(l1->val <= l2->val){
+        }
+        if(l1->val <= l2->val){
         l1->next = mergeTwoLists(l1->next, l2);
         return l1;
-    }
-    else{
+        }
+        else {
         l2->next = mergeTwoLists(l1, l2->next);
         return l2;
+        }
     }
-}
 };
