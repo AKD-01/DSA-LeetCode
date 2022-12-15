3a5d96115d70dcfb6f4d91a54e38ee2ff8deb045
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2022-12-15 10:25:50 +0530

    Time: 18 ms (84.85%), Space: 10.6 MB (79.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,16 +4,11 @@ public:
         int m=x.length();
         int n=y.length();
         int dp[m+1][n+1];
-        for(int i=0;i<=m;i++) dp[i][0]=0;
-        for(int j=0;j<=n;j++) dp[0][j]=0;
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=n;j++) {
-                if(x[i-1]==y[j-1]) {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else {
-                   dp[i][j]=max(dp[i-1][j], dp[i][j-1]); 
-                }
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                if(i==0 || j==0) dp[i][j]=0;
+                else if(x[i-1]==y[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]); 
             }
         }
         return dp[m][n];
