39c8ab6c9c8822fd27ff7bc5e037196993b0c749
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2022-12-17 10:06:54 +0530

    Time: 15 ms (76.77%), Space: 12 MB (78.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-    stack<int> stn;
-    for(auto s:tokens) {
-        if(s.size()>1 || isdigit(s[0])) stn.push(stoi(s));
-        else {
-            long x2=stn.top(); stn.pop();
-            auto x1=stn.top(); stn.pop();
-            switch(s[0]) {
-                case '+': x1+=x2; break;
-                case '-': x1-=x2; break;
-                case '*': x1*=x2; break;
-                case '/': x1/=x2; break;
+        stack<int> stk;
+        for(auto s: tokens){
+            if(s.size()>1 || isdigit(s[0])) stk.push(stoi(s));
+            else {
+                auto x2=stk.top(); 
+                stk.pop();
+                long x1=stk.top();
+                stk.pop();
+                switch(s[0]) {
+                        case '+': x1+=x2;
+                        break;
+                        case '-': x1-=x2;
+                        break;
+                        case '*': x1*=x2;
+                        break;
+                        case '/': x1/=x2;
+                        break;
+                }
+                stk.push(x1);
             }
-            stn.push(x1);
         }
-    }
-    return stn.top();
+        return stk.top();
     }
 };
