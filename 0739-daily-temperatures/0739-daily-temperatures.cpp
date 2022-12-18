fea59febb2120b256f595ae236dea5bb99339650
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2022-12-18 09:06:33 +0530

    Time: 285 ms (69.88%), Space: 98.7 MB (74.20%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
-        vector<int> ans(n, 0); // initially all 0, stores distance between their next greater element and current temperature
-        stack<int>st{};
-		// move from right to left
+        vector<int> ans(n, 0); 
+        stack<int> st;
         for(int i = n-1; i>=0; --i) {
-            // pop until we find next greater element to the right
-			// since we came from right stack will have element from right only
-			// s.top() is the index of elements so we put that index inside temperatures vector to check
             while(!st.empty() && temperatures[st.top()] <= temperatures[i])
                 st.pop();
-				
-            // if stack not empty, then we have some next greater element, 
-			// so we take distance between next greater and current temperature
-			// as we are storing indexes in the stack
             if(!st.empty())
-                ans[i] = st.top()-i; // distance between next greater and current
-            
-			// push the index of current temperature in the stack,
-			// same as pushing current temperature in stack
+                ans[i] = st.top()-i; 
             st.push(i);
         }
         return ans;  
