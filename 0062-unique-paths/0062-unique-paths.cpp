261e1548679f6f0f6840a32c8c84eeb76a828653
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-01-02 22:59:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (71.44%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int r = m-1; // r=n-1 then tc:O(n-1)
+        double res=1;
+        for(int i=1; i<=r; i++) {
+            res = res * (N-r+i)/i;
+        }
+        return (int)res;
+    }
+};
+
+//TC: O(m-1)
+//SC: O(1)
+
+/*class Solution {
+public:
     int countpaths(int i, int j, int m, int n, vector<vector<int>> &dp) {
         if(i==m-1 && j==n-1) return 1;
         if(i>=m || j>=n) return 0;
@@ -13,3 +29,6 @@ public:
         return dp[0][0];
     }
 };
+
+TC : O(m*n);
+SC : O(m*n); */
