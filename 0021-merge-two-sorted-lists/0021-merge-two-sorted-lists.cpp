e681302eb3995c233cc9bb14b683b472fe8c91d0
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-01-17 11:17:45 +0530

    Time: 4 ms (94.46%), Space: 14.7 MB (98.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,20 +11,25 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode dummy(INT_MIN);
-        ListNode *tail = &dummy;
-        while (l1 && l2) {
-            if(l1->val < l2->val) {
-                tail->next = l1;
+        // when list1 is empty then our output will be same as list2
+        if(l1 == NULL) return l2; 
+        // when list2 is empty then our output will be same as list1
+        if(l2 == NULL) return l1;
+        // pointing l1 and l2 to smallest and greatest one
+        if(l1->val > l2->val) std::swap(l1,l2);
+        // act as head of resultant merged list
+        ListNode* res = l1;
+        while(l1 != NULL && l2 != NULL) {
+            ListNode* temp = NULL;
+            while(l1 != NULL && l1->val <= l2->val) {
+                temp = l1;//storing last sorted node  
                 l1 = l1->next;
-            } 
-            else {
-                tail->next = l2;
-                l2 = l2->next;
             }
-            tail = tail->next;
+            // link previous sorted node with 
+            // next larger node in list2
+            temp->next = l2;
+            std::swap(l1,l2);
         }
-        tail->next = l1 ? l1 : l2;
-        return dummy.next;
+        return res;
     }
 };
