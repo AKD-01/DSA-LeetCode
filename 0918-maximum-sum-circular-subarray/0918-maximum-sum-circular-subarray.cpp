c3786fda3927d83034293ee425d8c154e2fb0e30
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-01-18 08:29:52 +0530

    Time: 68 ms (83.17%), Space: 39.8 MB (77.39%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+        for (int& a : A) {
+            curMax = max(curMax + a, a);
+            maxSum = max(maxSum, curMax);
+            curMin = min(curMin + a, a);
+            minSum = min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? max(maxSum, total - minSum) : maxSum;
+    }
+};
