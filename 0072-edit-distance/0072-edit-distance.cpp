c035420c822f4371f6d954cf0a852a314cd779a1
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-01-21 09:42:42 +0530

    Time: 11 ms (82.46%), Space: 6.3 MB (98.99%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size(), pre;
+        vector<int> cur(n + 1, 0);
+        for (int j = 1; j <= n; j++) {
+            cur[j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            pre = cur[0];
+            cur[0] = i;
+            for (int j = 1; j <= n; j++) {
+                int temp = cur[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    cur[j] = pre;
+                } else {
+                    cur[j] = min(pre, min(cur[j - 1], cur[j])) + 1;
+                }
+                pre = temp;
+            }
+        }
+        return cur[n];
+    }
+};
