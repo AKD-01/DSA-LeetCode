4fc82231f4832144421a69c07631bc11ebfd2982
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-01-31 06:27:09 +0530

    Time: 189 ms (75.00%), Space: 19.3 MB (81.82%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        const int n = scores.size();
+        vector<int> dp(n, 0);
+        // first: age
+        // second: scores
+        vector<pair<int, int>> arr(n, {0, 0}); 
+        for(int i = 0; i < n; ++i){
+            arr[i].first = ages[i];
+            arr[i].second = scores[i];
+        }
+        sort(arr.begin(), arr.end());
+        int team_score = 0;
+        for(int i = 0; i < n; ++i){
+            dp[i] = arr[i].second;
+            for(int j = i - 1; j >= 0; --j){
+                // arr sorted by age, latter element has to have high score to maintain increasing susequence
+                if(arr[i].second >= arr[j].second){
+                    // update each status
+                    dp[i] = max(dp[i], dp[j] + arr[i].second); 
+                }
+            }
+            // maximum value could in any place
+            team_score = max(team_score, dp[i]); 
+        }
+        return team_score;
+    }
+};
