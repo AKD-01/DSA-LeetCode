02f84ae2b079618ccf0eb0e75a7f477fb716b04e
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-06 17:19:27 +0530

    Time: 11 ms (79.98%), Space: 18.4 MB (78.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        //brute force approach tc-O(nlogn + n) and sc-O(n)
-        set<int> s;
+        //optimized approach tc-O(n) sc-O(1) as two pointers approach
+        int i=0, n=nums.size();
+        for(int j=1; j<n; j++) {
+            if(nums[i] != nums[j]) {
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+};
+
+//brute force approach tc-O(nlogn + n) and sc-O(n)
+        /*set<int> s;
         for(auto it : nums) {
             s.insert(it);
         }
@@ -11,6 +23,4 @@ public:
             nums[index] = it;
             index++;
         }
-        return index;
-    }
-};
+        return index;*/
