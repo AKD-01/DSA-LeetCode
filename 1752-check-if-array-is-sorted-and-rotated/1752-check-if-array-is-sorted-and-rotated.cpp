5954ec63eb9e60dfd9b8c20e5a9939292e11fbc8
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-06 17:09:40 +0530

    Time: 3 ms (56.94%), Space: 8.5 MB (10.08%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
+        //there will be atmost one position that nums[i]>nums[i+1] if array is sorted
         int count=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]>nums[(i+1)%n]) count++;
+            if(nums[i]>nums[(i+1)%n]) count++; //i+1%n is done to compare the first element with the last element i.e nums[n-1] with nums[0].
         }
-        
         return count<=1;
     }
 };
