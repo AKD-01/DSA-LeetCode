17500fb69a5615a74eef00a8c31a9896b63e365f
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-14 14:02:15 +0530

    Time: 37 ms (34.66%), Space: 25 MB (60.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     void rotate(vector<int> &nums, int k) {
+        //Optimized TC-O(2n) and SC=O(1)
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
        //Brute TC-O(n+k) SC-O(k)
-        vector<int> temp;
+        /*vector<int> temp;
         int n=nums.size();
         k=k%n;
         for(int i=n-k;i<n;i++) {
@@ -15,6 +21,6 @@ public:
         //put back temp elements
         for(int i=0;i<k;i++) {
             nums[i]=temp[i];
-        }
+        }*/
     }
 };
