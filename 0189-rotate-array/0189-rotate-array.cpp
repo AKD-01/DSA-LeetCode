7d844c0cb37fef280d7f99f832c141f7adc8c201
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-14 13:55:20 +0530

    Time: 25 ms (93.86%), Space: 25.6 MB (30.36%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     void rotate(vector<int> &nums, int k) {
-        k %=nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin()+k);
-        reverse(nums.begin()+k, nums.end());
+       //Brute TC-O(n+k) SC-O(k)
+        vector<int> temp;
+        int n=nums.size();
+        k=k%n;
+        for(int i=n-k;i<n;i++) {
+            temp.push_back(nums[i]);
+        }
+        //Shifting
+        for(int i=n-1;i>=k;i--) {
+            nums[i]=nums[i-k];        
+        }
+        //put back temp elements
+        for(int i=0;i<k;i++) {
+            nums[i]=temp[i];
+        }
     }
 };
