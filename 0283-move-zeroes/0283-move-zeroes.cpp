22acdd9e3dc87167bad3b5832e7c82bb35b2e238
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-15 10:20:32 +0530

    Time: 26 ms (55.88%), Space: 19.3 MB (65.70%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        //Optimized Approach TC-0(N) SC-O(1)
+        int j=-1;
+        int n=nums.size();
+        for(int i=0; i<n; i++) {
+           if(nums[i]==0) {
+               j=i;
+               break;
+           } 
+        }
+        if(j==-1)  return;
+        for(int i=j+1;i<n;i++) {
+            if(nums[i]!=0) {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
         //Brute force approach TC-O(2N) SC-O(x) x-non zero elements
-        vector<int> temp;
+        /*vector<int> temp;
         int n =  nums.size();
         for(int val: nums) {
             if(val!=0) temp.push_back(val);
@@ -13,6 +29,6 @@ public:
         }
         for(int i=s; i<n; i++) {
             nums[i]=0;
-        }
+        }*/
     }
 };
