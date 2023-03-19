15fe64f12ebebcf11d5cc42f823665bce1615810
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-03-19 20:59:08 +0530

    Time: 12 ms (81.61%), Space: 10.7 MB (44.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //Optimized approach using hashmap TC-O(n) and SC-O(n)
         unordered_map<int,int> m;
         int n=nums.size();
         for(int i=0; i<n; i++) {
