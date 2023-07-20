8b7f89bfc2e124343e7aef8dc3cb99e688f7c999
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2023-07-20 06:52:43 +0530

    Time: 19 ms (38.66%), Space: 17.8 MB (24.17%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
+            }
+            else {
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() and s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0) {
+                        s.push(ast[i]);
+                    }
+                }
+            }
+        }
+		// finally we are returning the elements which remains in the stack.
+		// we have to return them in reverse order.
+        vector<int> res(s.size());
+        for(int i = (int)s.size() - 1; i >= 0; i--) {
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
+    }
+};
