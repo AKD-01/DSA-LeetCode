4f2bee87ef1f4834ec8af6bceddee5dc4b8bcec9
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-05-23 07:54:47 +0530

    Time: 125 ms (68.18%), Space: 43 MB (9.30%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    const int MX_NUM = 1000;
+    int find(int index, vector<int>& nums, int k, vector<int> &cnt) {
+        if(index == nums.size()) {
+            return 1;
+        }
+        int ans = find(index + 1, nums, k, cnt); //don't take nums[index]
+        int num = nums[index];
+
+        if((num + k > MX_NUM || cnt[num + k] == 0) && (num - k < 1 || cnt[num - k] == 0)) {
+            cnt[num]++;
+            ans += find(index + 1, nums, k, cnt);
+            cnt[num]--;
+        }
+        return ans;
+    }
+    
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        vector<int> cnt(MX_NUM + 1, 0);
+        return find(0, nums, k, cnt) - 1;
+    }
+};
