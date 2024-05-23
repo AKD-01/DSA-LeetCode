e0f9d6e314d57479dabd238e7255464c2453c6a4
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-05-23 07:55:05 +0530

    Time: 119 ms (69.01%), Space: 43 MB (9.92%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -4,7 +4,7 @@ class Solution {
         if(index == nums.size()) {
             return 1;
         }
-        int ans = find(index + 1, nums, k, cnt); //don't take nums[index]
+        int ans = find(index + 1, nums, k, cnt); 
         int num = nums[index];
 
         if((num + k > MX_NUM || cnt[num + k] == 0) && (num - k < 1 || cnt[num - k] == 0)) {
