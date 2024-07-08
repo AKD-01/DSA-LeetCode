9c93716d8bf122a4d67f8cddde1a626408232925
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-07-08 07:11:44 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (76.96%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int helper(int n,int k){
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            ans = (ans + k) % i;
+        }
+        return ans;
+    }
+    int findTheWinner(int n, int k) {
+        return helper(n,k)+1;   
+    }
+};
