1d528970fd22dc3a052247dbb05b10cfdac066be
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-07-10 07:20:59 +0530

    Time: 6 ms (40.12%), Space: 14.3 MB (17.13%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        for (string &s : logs)
+        {
+            if (s == "../")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else if (s == "./")
+                continue;
+            else
+                st.push(s);
+        }
+        return st.size();
+    }
+};
