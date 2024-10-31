a44de860fa5ce615b629d424fd8a99a57186e649
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-10-31 09:11:03 +0530

    Time: 39 ms (82.76%), Space: 10.8 MB (98.13%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long dp[101][101] = {};
+long long dfs(int i, int j, vector<vector<int>>& f, vector<int>& r) {
+    if (j >= r.size())
+        return 0;
+    if (i >= f.size())
+        return 200000000000;
+    if (dp[i][j] == 0) {
+        dp[i][j] = 1 + dfs(i + 1, j, f, r);
+        long long dist = 0;
+        for (int n = 0; j + n < r.size() && n < f[i][1]; ++n) {
+            dist += abs(f[i][0] - r[j + n]);
+            dp[i][j] = min(dp[i][j], 1 + dist + dfs(i + 1, j + n + 1, f, r));
+        }
+    }
+    return dp[i][j] - 1;
+}
+long long minimumTotalDistance(vector<int>& r, vector<vector<int>>& f) {
+    sort(begin(r), end(r));
+    sort(begin(f), end(f));
+    return dfs(0, 0, f, r);
+}
+};
