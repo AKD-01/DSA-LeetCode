78191511ee072e3bcda02b0a0a690e25bca6eb3d
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-11-14 07:11:50 +0530

    Time: 24 ms (89.89%), Space: 87.2 MB (72.13%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& A) {
+      int left = 1, right = 100000;
+        while (left < right) {
+            int mid = (left + right) / 2, sum = 0;
+            for (int a : A)
+                sum += (a + mid - 1) / mid;
+            if (sum > n)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;  
+    }
+};
