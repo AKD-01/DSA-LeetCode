3409c15a306c14137cee7f536bd0fae0e5826a60
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2024-11-18 06:27:32 +0530

    Time: 235 ms (5.02%), Space: 10.5 MB (17.93%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+   void addNums(vector<int> &nums, vector<int> &code)
+    {
+        for(auto val:code) 
+            nums.push_back(val);
+    }
+    vector<int> decrypt(vector<int>& code, int k) 
+    {
+        int n = code.size();
+        vector<int>ans(n);
+        if(k == 0) return ans;
+        auto nums = code;
+        addNums(nums, code);
+        addNums(nums, code);
+
+        for(int i = n; i < n + n; i++)
+        {
+            int sum = 0;
+            if(k < 0)
+            {
+                for(int j = i - 1; j >= i - -k; j--)
+                {
+                    sum += nums[j];
+                    cout<<nums[j]<<" ";
+                }
+                cout<<endl;
+            }
+            else
+            {
+                for(int j = i + 1; j <= i + k; j++)
+                {
+                    sum += nums[j];
+                    cout<<nums[j]<<"";
+                }
+                cout<<endl;
+            }
+            ans[i - n] = sum;
+        }
+        return ans;
+    }
+};
