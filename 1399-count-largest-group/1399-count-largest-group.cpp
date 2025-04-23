8ecacb9f83fcb7f9481ee5bb729a563289875e9c
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-04-23 06:07:03 +0530

    Time: 3 ms (33.9%), Space: 8.3 MB (27.46%) - LeetHub

---
+++ 1399-count-largest-group/1399-count-largest-group.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        unordered_map<int, int> hashMap;
+        int maxValue = 0;
+        for (int i = 1; i <= n; ++i) {
+            int key = 0, i0 = i;
+            while (i0) {
+                key += i0 % 10;
+                i0 /= 10;
+            }
+            ++hashMap[key];
+            maxValue = max(maxValue, hashMap[key]);
+        }
+        int count = 0;
+        for (auto& [_, value] : hashMap) {
+            if (value == maxValue) {
+                ++count;
+            }
+        }
+        return count;
+    }
+};
