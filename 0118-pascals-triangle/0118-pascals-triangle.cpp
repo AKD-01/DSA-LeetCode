07400a836a3c597543121ae4c7b343ece8aec4df
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-08-01 07:33:49 +0530

    Time: 2 ms (15.16%), Space: 9.7 MB (30.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,49 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> r(numRows);
-        for(int i=0;i<numRows;i++) {
-            r[i].resize(i+1);
-            r[i][0]=r[i][i]=1;
-            for(int j=1;j<i;j++) {
-                r[i][j]=r[i-1][j-1]+r[i-1][j];
-            }
+    //Type-3 : Given n print the entire triangle
+    vector<int> generateRow(int row) {
+        long long ans=1;
+        vector<int> list;
+        list.push_back(ans);
+        for(int col=1; col<row; col++) {
+            ans*=row-col;
+            ans/=col;
+            list.push_back(ans);
         }
-        return r;
+        return list;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1; i<=numRows; i++) {
+            ans.push_back(generateRow(i));
+        }
+        return ans;
     }
 };
+
+// Type - 2 Print the given nth row of pascal triangle 
+// n=5 Ans - 1 3 3 1 
+// for(c=1;c<n;c++) {
+//     cout<<nCr(n-1,c-1);
+// }
+//or
+// long long ans = 1;
+// cout<<ans;
+// for(int i=1; i<n; i++) {
+//     ans=ans * n-i;
+//     ans=ans / i;
+//     cout<<ans;
+// }
+
+// Type - 1 Given R & C tell the element at that place.
+// int R=5, C=3; Ans : 4C2 = 6
+// int nCr(int n, int r) {
+//     long long res = 1;
+//     for(int i=0; i<r; i++) {
+//         res = res*n-i;
+//         res = res/i+1;
+//     }
+//     return res;
+// }
+
+// cout<<nCr(R-1, C-1);
