d4ab2b337e83d4e49dacc4f6a4bd4130690d5814
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-08-16 07:25:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.82%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-    //go through each digit of the number from least significant
-    //keep track of the most significant 6 digit seen
-    int p = 1;
-    int n = num;
-    int maxp = 0;
-    while(num>0) {
-		int digit = (num % 10);
-        if(digit==6) {
-			maxp = p;
-		}
-        p*=10;
-        num/=10;
-	}
-    //change this '6' to '9' by adding 3*max6bit position
-    n+=(maxp*3);
-    return n;
+        string str=to_string(num);
+        for(int i=0;i<str.size();i++) {
+            if(str[i]=='6') {
+                str[i]='9';
+                break;
+            }
+        }
+        return stoi(str);
     }
 };
