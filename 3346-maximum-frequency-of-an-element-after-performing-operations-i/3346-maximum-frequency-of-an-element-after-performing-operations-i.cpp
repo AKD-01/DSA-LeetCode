31db89ad1d0e116f5be209da98fb46ef8da97df6
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-10-21 09:01:24 +0530

    Time: 224 ms (47.82%), Space: 122.5 MB (49.76%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+        unordered_map<int, int> numCount;
+
+        int lastNumIndex = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[lastNumIndex]) {
+                numCount[nums[lastNumIndex]] = i - lastNumIndex;
+                ans = max(ans, i - lastNumIndex);
+                lastNumIndex = i;
+            }
+        }
+
+        numCount[nums[lastNumIndex]] = nums.size() - lastNumIndex;
+        ans = max(ans, (int)nums.size() - lastNumIndex);
+
+        auto leftBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] < value) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        };
+
+        auto rightBound = [&](int value) {
+            int left = 0, right = nums.size() - 1;
+            while (left < right) {
+                int mid = (left + right + 1) / 2;
+                if (nums[mid] > value) {
+                    right = mid - 1;
+                } else {
+                    left = mid;
+                }
+            }
+            return left;
+        };
+
+        for (int i = nums.front(); i <= nums.back(); i++) {
+            int l = leftBound(i - k);
+            int r = rightBound(i + k);
+
+            int tempAns;
+            if (numCount.count(i)) {
+                tempAns = min(r - l + 1, numCount[i] + numOperations);
+            } else {
+                tempAns = min(r - l + 1, numOperations);
+            }
+            ans = max(ans, tempAns);
+        }
+
+        return ans;
+    }
+};
