dd80104b2e2a30c9a58e2a6b1f3641671b639414
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-12-23 08:09:50 +0530

    Time: 199 ms (13.51%), Space: 196.1 MB (22.78%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        int res = 0, max_val = 0;
-        priority_queue<pair<int, int>> pq;
-        sort(begin(events), end(events));
-        for (auto &e : events) {
-            for(; !pq.empty() && -pq.top().first < e[0]; pq.pop())
-                max_val = max(max_val, pq.top().second);
-            res = max(res, max_val + e[2]);
-            pq.push({-e[1], e[2]});
+        sort(events.begin(), events.end());
+        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
+        return findEvents(events, 0, 0, dp);
+    }
+    int findEvents(vector<vector<int>>& events, int idx, int cnt,
+                   vector<vector<int>>& dp) {
+        if (cnt == 2 || idx >= events.size()) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.size() - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
+            }
+            int include =
+                events[idx][2] + (lo < events.size() && events[lo][0] > end
+                                      ? findEvents(events, lo, cnt + 1, dp)
+                                      : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = max(include, exclude);
         }
-        return res; 
+        return dp[idx][cnt];
     }
 };
