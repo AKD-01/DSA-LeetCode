6478e9c3804a38cd64fa047b6ef15244fa18c71a
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-12-27 08:25:36 +0530

    Time: 79 ms (46.92%), Space: 98.3 MB (36.1%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
+        vector<long long> roomAvailabilityTime(n, 0);
         vector<int> meetingCount(n, 0);
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> usedRooms;
-        priority_queue<int, vector<int>, greater<int>> unusedRooms;
-        for (int i = 0; i < n; i++) {
-            unusedRooms.push(i);
-        }
         sort(meetings.begin(), meetings.end());
 
-        for (auto meeting : meetings) {
+        for (auto& meeting: meetings) {
             int start = meeting[0], end = meeting[1];
+            long long minRoomAvailabilityTime = LLONG_MAX;
+            int minAvailableTimeRoom = 0;
+            bool foundUnusedRoom = false;
 
-            while (!usedRooms.empty() && usedRooms.top().first <= start) {
-                int room = usedRooms.top().second;
-                usedRooms.pop();
-                unusedRooms.push(room);
+            for (int i = 0; i < n; i++) {
+                if (roomAvailabilityTime[i] <= start) {
+                    foundUnusedRoom = true;
+                    meetingCount[i]++;
+                    roomAvailabilityTime[i] = end;
+                    break;
+                }
+
+                if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
+                    minRoomAvailabilityTime = roomAvailabilityTime[i];
+                    minAvailableTimeRoom = i;
+                }
             }
-            if (!unusedRooms.empty()) {
-                int room = unusedRooms.top();
-                unusedRooms.pop();
-                usedRooms.push({end, room});
-                meetingCount[room]++;
-            } else {
-                auto [roomAvailabilityTime, room] = usedRooms.top();
-                usedRooms.pop();
-                usedRooms.push({roomAvailabilityTime + end - start, room});
-                meetingCount[room]++;
+
+            if (!foundUnusedRoom) {
+                roomAvailabilityTime[minAvailableTimeRoom] += end - start;
+                meetingCount[minAvailableTimeRoom]++;
             }
         }
-
         int maxMeetingCount = 0, maxMeetingCountRoom = 0;
         for (int i = 0; i < n; i++) {
             if (meetingCount[i] > maxMeetingCount) {
