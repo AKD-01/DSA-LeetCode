18640ef5e98b920a3edc724a4b051ebcefa00c47
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-12-30 08:22:40 +0530

    Time: 0 ms (100%), Space: 11.4 MB (88.45%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -1,33 +1,61 @@
 class Solution {
 public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), res = 0;
-        if (m < 3 || n < 3) return 0;
-        for (int i = 0; i < m - 2; i++)
-            for (int j = 0; j < n - 2; j++)
-                res += isMagicSquare(i, j, grid);
-        return res;
-    }
-private:
-    bool isMagicSquare(int r, int c, vector<vector<int>>& grid){
-        int rows[3] = {}, cols[3] = {}, diag[2] = {};
-        bool s[10] = {};
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                int val = grid[r + i][c + j];
-                if (val < 1 || val > 9 || s[val])
-                    return false;
-                s[val] = true;
-                rows[i] += val;
-                cols[j] += val;
-                if (i == j)
-                    diag[0] += val;
-                if (i + j == 2)
-                    diag[1] += val;
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int row = 0; row + 2 < m; row++) {
+            for (int col = 0; col + 2 < n; col++) {
+                if (isMagicSquare(grid, row, col)) {
+                    ans++;
+                }
             }
         }
-        return (rows[0] == rows[1] && rows[1] == rows[2]) && 
-               (cols[0] == cols[1] && cols[1] == cols[2]) && 
-               (diag[0] == diag[1]);
+        return ans;
+    }
+
+private:
+    bool isMagicSquare(vector<vector<int>>& grid, int row, int col) {
+        bool seen[10] = {false};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int num = grid[row + i][col + j];
+                if (num < 1 || num > 9) return false;
+                if (seen[num]) return false;
+                seen[num] = true;
+            }
+        }
+
+        // Check if diagonal sums are the same
+        int diagonal1 =
+            grid[row][col] + grid[row + 1][col + 1] + grid[row + 2][col + 2];
+        int diagonal2 =
+            grid[row + 2][col] + grid[row + 1][col + 1] + grid[row][col + 2];
+
+        if (diagonal1 != diagonal2) return false;
+
+        // Check if all row sums are the same as the diagonal sums
+        int row1 = grid[row][col] + grid[row][col + 1] + grid[row][col + 2];
+        int row2 = grid[row + 1][col] + grid[row + 1][col + 1] +
+                   grid[row + 1][col + 2];
+        int row3 = grid[row + 2][col] + grid[row + 2][col + 1] +
+                   grid[row + 2][col + 2];
+
+        if (!(row1 == diagonal1 && row2 == diagonal1 && row3 == diagonal1)) {
+            return false;
+        }
+
+        // Check if all column sums are the same as the diagonal sums
+        int col1 = grid[row][col] + grid[row + 1][col] + grid[row + 2][col];
+        int col2 = grid[row][col + 1] + grid[row + 1][col + 1] +
+                   grid[row + 2][col + 1];
+        int col3 = grid[row][col + 2] + grid[row + 1][col + 2] +
+                   grid[row + 2][col + 2];
+
+        if (!(col1 == diagonal1 && col2 == diagonal1 && col3 == diagonal1)) {
+            return false;
+        }
+
+        return true;
     }
 };
