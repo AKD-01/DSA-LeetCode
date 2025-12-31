08bcfac8de349627725c68d82672ba5896556a8a
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2025-12-31 08:08:45 +0530

    Time: 261 ms (42.93%), Space: 193.1 MB (56.02%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -5,30 +5,30 @@ public:
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (canWalk(cells, row, col, mid)) {
-                ans = mid; // Update current answer so far
-                left = mid + 1; // Try to find a larger day
-            } else right = mid - 1; // Try to find a smaller day
+                ans = mid;
+                left = mid + 1; 
+            } else right = mid - 1; 
         }
         return ans;
     }
     int DIR[5] = {0, 1, 0, -1, 0};
     bool canWalk(vector<vector<int>>& cells, int row, int col, int dayAt) {
-        vector<vector<bool>> grid(row, vector<bool>(col, 0)); // Create grid in the `dayAt` th date
+        vector<vector<bool>> grid(row, vector<bool>(col, 0)); 
         for (int i = 0; i < dayAt; ++i) grid[cells[i][0]-1][cells[i][1]-1] = 1;
         queue<pair<int, int>> bfs;
         for (int c = 0; c < col; ++c) {
-            if (grid[0][c] == 0) { // Add all valid start cells in the top row
+            if (grid[0][c] == 0) {
                 bfs.push({0, c});
-                grid[0][c] = 1; // Mark as visited
+                grid[0][c] = 1; 
             }
         }
         while (!bfs.empty()) {
             auto [r, c] = bfs.front(); bfs.pop();
-            if (r == row - 1) return true; // Reach to bottom -> Return Valid
+            if (r == row - 1) return true; 
             for (int i = 0; i < 4; ++i) {
                 int nr = r + DIR[i], nc = c + DIR[i+1];
                 if (nr < 0 || nr == row || nc < 0 || nc == col || grid[nr][nc] == 1) continue;
-                grid[nr][nc] = 1; // Mark as visited
+                grid[nr][nc] = 1; 
                 bfs.push({nr, nc});
             }
         }
