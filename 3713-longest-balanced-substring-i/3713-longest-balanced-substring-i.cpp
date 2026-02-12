1b8237b2d4e1eaea5fd96a671d5143670a69e518
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-02-12 08:52:10 +0530

    Time: 142 ms (74.9%), Space: 11.4 MB (96.34%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size();
+        int res = 0;
+        vector<int> cnt(26);
+        for (int i = 0; i < n; i++) {
+            fill(cnt.begin(), cnt.end(), 0);
+            for (int j = i; j < n; j++) {
+                bool flag = true;
+                int c = s[j] - 'a';
+                cnt[c]++;
+                for (auto x : cnt) {
+                    if (x > 0 && x != cnt[c]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    res = max(res, j - i + 1);
+                }
+            }
+        }
+        return res;
+    }
+};
