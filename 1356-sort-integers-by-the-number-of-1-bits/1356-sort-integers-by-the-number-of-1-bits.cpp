c0d71b5fca84ac37cb217b448d49ef76031a7043
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-02-25 09:04:34 +0530

    Time: 3 ms (47.07%), Space: 14.1 MB (79.86%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
+    static bool compare(int a, int b) {
+        if (__builtin_popcount(a) == __builtin_popcount(b)) {
+            return a < b;
+        }
+        
+        return __builtin_popcount(a) < __builtin_popcount(b);
+    }
+    
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(), arr.end(),[](const int& a, const int& b){
-            int countA = __builtin_popcount(a), countB = __builtin_popcount(b);
-            return countA==countB ? a<b:countA<countB;});
+        sort(arr.begin(), arr.end(), compare);
         return arr;
     }
 };
