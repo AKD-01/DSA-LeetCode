71ede4f16558c816eca961d02bd4288cdfb9a452
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-02-26 06:31:44 +0530

    Time: 0 ms (100%), Space: 9.4 MB (18.91%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
+    void divideByTwo(string& s) { s.pop_back(); }
+
+    void addOne(string& s) {
+        int i = s.size() - 1;
+
+        while (i >= 0 && s[i] != '0') {
+            s[i] = '0';
+            i--;
+        }
+
+        if (i < 0) {
+            s = '1' + s;
+        } else {
+            s[i] = '1';
+        }
+    }
+
     int numSteps(string s) {
-      int step = 0;
-        while (s != "1") {
-            if (s.back() == '0') s.pop_back();
-		else {
-			while (!s.empty() && s.back() == '1') {
-				s.pop_back();
-				++step;
-			}
-			if (s.empty()) return step + 1;
-			else s.back() = '1';
-		}
-		++step;
-	}
-	return step;  
+        int N = s.size();
+
+        int operations = 0;
+        while (s.size() > 1) {
+            N = s.size();
+
+            if (s[N - 1] == '0') {
+                divideByTwo(s);
+            } else {
+                addOne(s);
+            }
+
+            operations++;
+        }
+
+        return operations;
     }
 };
