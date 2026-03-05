df28029675f0ac3ca21b5578d8b27dbb2e3106df
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-03-05 07:12:24 +0530

    Time: 0 ms (100%), Space: 9.4 MB (76.2%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int res = 0, n = s.length();
-        for (int i = 0; i < n; ++i)
-            if (s[i] - '0' != i % 2)
-                res++;
-        return min(res, n - res);
+        int start0 = 0;
+        int start1 = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (i % 2 == 0) {
+                if (s[i] == '0') {
+                    start1++;
+                } else {
+                    start0++;
+                }
+            } else {
+                if (s[i] == '1') {
+                    start1++;
+                } else {
+                    start0++;
+                }
+            }
+        }
+        
+        return min(start0, start1);
     }
 };
