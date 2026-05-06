e757860e0b820b539744ac19d4937311997bb166
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-05-06 07:52:42 +0530

    Time: 56 ms (11.06%), Space: 56.7 MB (90.17%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-      int row = box.size();
-        int col = box[0].size();
-        vector<vector<char>> box_90(col, vector<char>(row));
+        int m = box.size();
+        int n = box[0].size();
+        vector<vector<char>> result(n, vector<char>(m));
 
-        for (int i = 0; i < row; i++) {
-            int cell = col - 1;
-            for (int j = col - 1; j >= 0; j--) {
-                if (box[i][j] == '*') {
-                    cell = j - 1;
-                } else if (box[i][j] == '#') {
-                    box[i][j] = '.';
-                    box[i][cell--] = '#';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result[i][j] = box[j][i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            reverse(result[i].begin(), result[i].end());
+        }
+
+        for (int j = 0; j < m; j++) {
+            for (int i = n - 1; i >= 0; i--) {
+                if (result[i][j] == '.') {  
+                    int nextRowWithStone = -1;
+
+                    for (int k = i - 1; k >= 0; k--) {
+                        if (result[k][j] == '*')
+                            break;  
+                        if (result[k][j] ==
+                            '#') {  
+                            nextRowWithStone = k;
+                            break;
+                        }
+                    }
+                    if (nextRowWithStone != -1) {
+                        result[nextRowWithStone][j] = '.';
+                        result[i][j] = '#';
+                    }
                 }
             }
         }
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                box_90[j][row - i - 1] = box[i][j];
-            }
-        }
-
-        return box_90;  
+        return result;
     }
 };
