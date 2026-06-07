97dfcea173cb50fa6f6a1cd9745d39454e9b0c1d
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-06-07 07:40:08 +0530

    Time: 311 ms (15.38%), Space: 321.1 MB (5.12%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -1,35 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* createBinaryTree(vector<vector<int>>& desp) {
-        unordered_map<int, TreeNode*> m;
-        unordered_map<int, bool> root;
-        int r;
-        
-        for(auto d : desp) root[d[0]] = root[d[1]] = true;
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        // Sets to track unique children and parents
+        unordered_set<int> children, parents;
+        // Map to store parent to children relationships
+        unordered_map<int, vector<pair<int, int>>> parentToChildren;
 
-        for(auto d : desp) {
-            root[d[1]] = false;
-            TreeNode *par, *node;
-            m.count(d[0]) ? par = m[d[0]] : par = new TreeNode(d[0]), m[d[0]]=par;
-            m.count(d[1]) ? node = m[d[1]] : node = new TreeNode(d[1]), m[d[1]]=node;
-            d[2] ? par->left = node : par->right = node;  
+        // Build graph from parent to child, and add nodes to HashSets
+        for (auto& d : descriptions) {
+            int parent = d[0], child = d[1], isLeft = d[2];
+            parents.insert(parent);
+            parents.insert(child);
+            children.insert(child);
+            parentToChildren[parent].emplace_back(child, isLeft);
         }
-        
-        for(auto x : root)
-            if(x.second)
-                r = x.first;
-        
-        return m[r];
+
+        // Find the root node by checking which node is in parents but not in
+        // children
+        for (auto it = parents.begin(); it != parents.end();) {
+            if (children.find(*it) != children.end()) {
+                it = parents.erase(it);
+            } else {
+                ++it;
+            }
+        }
+        TreeNode* root = new TreeNode(*parents.begin());
+
+        // Starting from root, use BFS to construct binary tree
+        queue<TreeNode*> queue;
+        queue.push(root);
+
+        while (!queue.empty()) {
+            TreeNode* parent = queue.front();
+            queue.pop();
+            // Iterate over children of current parent
+            for (auto& childInfo : parentToChildren[parent->val]) {
+                int childValue = childInfo.first, isLeft = childInfo.second;
+                TreeNode* child = new TreeNode(childValue);
+                queue.push(child);
+                // Attach child node to its parent based on isLeft flag
+                if (isLeft == 1) {
+                    parent->left = child;
+                } else {
+                    parent->right = child;
+                }
+            }
+        }
+
+        return root;
     }
 };
