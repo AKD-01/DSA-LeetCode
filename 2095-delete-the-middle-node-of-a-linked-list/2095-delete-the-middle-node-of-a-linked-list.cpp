ed8bf8868bb74b7317e54ad3eb2d05f77a8ab2f8
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-06-15 06:37:56 +0530

    Time: 8 ms (8.52%), Space: 312.1 MB (54.78%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,34 +1,21 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if (head == NULL) {
-          return NULL;
-       }
-       if (head->next == NULL) {
-          delete head;
-          return NULL;
-       }
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
         ListNode* slow = head;
         ListNode* fast = head;
-        ListNode* prev = head; // keeping this prev node to get the previous node of slow pointer when we reach middle
-        while(fast != NULL && fast->next != NULL)
-        {
-            if(slow != head) prev = prev->next; // since we want prev to start one step later than slow node
+        ListNode* prev = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
+
         prev->next = slow->next;
-        delete slow;
         return head;
     }
 };
