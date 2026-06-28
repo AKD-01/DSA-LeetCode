ab3552319a04aaf9bad0706365b78610627c438c
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-06-28 08:16:05 +0530

    Time: 11 ms (41.24%), Space: 55.2 MB (69.21%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& A) {
-        sort(A.begin(), A.end());
-        int pre = 0;
-        for (int& a : A)
-            pre = min(pre + 1, a);
-        return pre;
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int ans = 1;
+        
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] >= ans + 1) {
+                ans++;
+            }
+        }
+        
+        return ans;
     }
 };
