c020dbc980d4a176d5dbcaf3076f81aaaa523621
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-07-01 08:20:11 +0530

    Time: 427 ms (60.96%), Space: 155.7 MB (59.31%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.cpp
@@ -1,34 +1,97 @@
 class Solution {
 public:
-    int maximumSafenessFactor(vector<vector<int>>& g) {
-    queue<array<int, 2>> q;
-    int dir[5] = {1, 0, -1, 0, 1}, n = g.size();
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            if (g[i][j])
-                q.push({i, j});
-    while (!q.empty()) {
-        auto [i, j] = q.front(); q.pop();
-        for (int d = 0; d < 4; ++d) {
-            int x = i + dir[d], y = j + dir[d + 1];
-            if (min(x, y) >= 0 && max(x, y) < n && g[x][y] == 0) {
-                g[x][y] = g[i][j] + 1;
-                q.push({x, y});
+
+    int maximumSafenessFactor(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        queue<pair<int, int>> multiSourceQueue;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    multiSourceQueue.push({i, j});
+                    grid[i][j] = 0;
+                } else {
+                    grid[i][j] = -1;
+                }
             }
         }
-    }
-    priority_queue<array<int, 3>> pq;
-    pq.push({g[0][0], 0, 0});
-    while (pq.top()[1] < n - 1 || pq.top()[2] < n - 1) {
-        auto [sf, i, j] = pq.top(); pq.pop();
-        for (int d = 0; d < 4; ++d) {
-            int x = i + dir[d], y = j + dir[d + 1];
-            if (min(x, y) >= 0 && max(x, y) < n && g[x][y] > 0) {
-                pq.push({min(sf, g[x][y]), x, y});
-                g[x][y] *= -1; 
+
+        while (!multiSourceQueue.empty()) {
+            int size = multiSourceQueue.size();
+            while (size-- > 0) {
+                auto curr = multiSourceQueue.front();
+                multiSourceQueue.pop();
+                for (auto& d : dir) {
+                    int di = curr.first + d[0];
+                    int dj = curr.second + d[1];
+                    int val = grid[curr.first][curr.second];
+                    if (isValidCell(grid, di, dj) && grid[di][dj] == -1) {
+                        grid[di][dj] = val + 1;
+                        multiSourceQueue.push({di, dj});
+                    }
+                }
             }
         }
+
+        int start = 0;
+        int end = 0;
+        int res = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                end = max(end, grid[i][j]);
+            }
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isValidSafeness(grid, mid)) {
+                res = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return res;
     }
-    return pq.top()[0] - 1;
-}
+
+private:
+
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    bool isValidCell(vector<vector<int>>& grid, int i, int j) {
+        int n = grid.size();
+        return i >= 0 && j >= 0 && i < n && j < n;
+    }
+
+    bool isValidSafeness(vector<vector<int>>& grid, int minSafeness) {
+        int n = grid.size();
+
+        if (grid[0][0] < minSafeness || grid[n - 1][n - 1] < minSafeness) {
+            return false;
+        }
+
+        queue<pair<int, int>> traversalQueue;
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        traversalQueue.push({0, 0});
+        visited[0][0] = true;
+
+        while (!traversalQueue.empty()) {
+            auto curr = traversalQueue.front();
+            traversalQueue.pop();
+            if (curr.first == n - 1 && curr.second == n - 1) {
+                return true; 
+            }
+            for (auto& d : dir) {
+                int di = curr.first + d[0];
+                int dj = curr.second + d[1];
+                if (isValidCell(grid, di, dj) && !visited[di][dj] &&
+                    grid[di][dj] >= minSafeness) {
+                    visited[di][dj] = true;
+                    traversalQueue.push({di, dj});
+                }
+            }
+        }
+
+        return false; 
+    }
 };
