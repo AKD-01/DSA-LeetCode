213e2145d84966f8261dda7ad02bae9ea0b63575
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-07-11 08:31:24 +0530

    Time: 107 ms (7.53%), Space: 143.1 MB (13.5%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for (auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        vector<vector<int>> graph(n);
+        unordered_map<string, int> componentFreq;
+
+        for (int vertex = 0; vertex < n; vertex++) {
+            graph[vertex].push_back(vertex);
         }
 
-        vector<bool> vis(n, false);
-        int count = 0;
+        for (const auto& edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                vector<int> comp;
-                queue<int> q;
-                q.push(i);
-                vis[i] = true;
+        for (int vertex = 0; vertex < n; vertex++) {
+            vector<int> neighbors = graph[vertex];
+            sort(neighbors.begin(), neighbors.end());
 
-                while (!q.empty()) {
-                    int cur = q.front();
-                    q.pop();
-                    comp.push_back(cur);
+            string key;
+            for (int num : neighbors) {
+                key += to_string(num) + ",";
+            }
+            componentFreq[key]++;
+        }
 
-                    for (int nei : adj[cur]) {
-                        if (!vis[nei]) {
-                            q.push(nei);
-                            vis[nei] = true;
-                        }
-                    }
-                }
-
-                bool complete = true;
-                for (int node : comp) {
-                    if (adj[node].size() != comp.size() - 1) {
-                        complete = false;
-                        break;
-                    }
-                }
-
-                if (complete) count++;
+        int completeCount = 0;
+        for (const auto& entry : componentFreq) {
+            int size = count(entry.first.begin(), entry.first.end(), ',');
+            if (size == entry.second) {
+                completeCount++;
             }
         }
 
-        return count;
+        return completeCount;
     }
 };
