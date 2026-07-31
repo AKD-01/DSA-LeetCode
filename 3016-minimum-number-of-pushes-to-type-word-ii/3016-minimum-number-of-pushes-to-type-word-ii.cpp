6959f4677b9e9f8ad1b0291a5fab1e5af291857e
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-07-31 07:54:14 +0530

    Time: 12 ms (42.94%), Space: 28.3 MB (33.24%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-       vector<int> freq(26,0);
-        int n=word.size(),count=0,ans=0;
-        for(int i=0;i<n;i++){
-            freq[word[i]-'a']++;
+        vector<int> frequency(26, 0);
+
+        for (char& c : word) {
+            ++frequency[c - 'a'];
         }
-        sort(freq.rbegin(),freq.rend());
-        for(int i=0;i<26;i++){
-            if(freq[i]==0) break;
-            if(count<=7){
-                ans+=freq[i];
-            }
-            else if(count<=15){
-                ans+=freq[i]*2;
-            }
-            else if(count<=23){
-                ans+=freq[i]*3;
-            }
-            else{
-                ans+=freq[i]*4;
-            }
-            count++;
+
+        sort(frequency.rbegin(), frequency.rend());
+
+        int totalPushes = 0;
+
+        for (int i = 0; i < 26; ++i) {
+            if (frequency[i] == 0) break;
+            totalPushes += (i / 8 + 1) * frequency[i];
         }
-        return ans; 
+
+        return totalPushes;
     }
 };
