a54b3ffc51cb0bdd973d189aea12370edae10147
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-08-09 08:51:33 +0530

    Time: 12 ms (42.49%), Space: 15 MB (31.15%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
+    int dfs(int ind, int m, vector<int>& piles, int n, vector<vector<int>>& dp) {
+        if (ind >= n) return 0;
+        if (dp[ind][m] != -1) return dp[ind][m];
+        int tot = 0, ans = INT_MIN;
+        for (int x = 0; x < 2 * m; x++) {
+            if (ind + x < n) tot += piles[ind + x];
+            ans = max(ans, tot - dfs(ind + x + 1, max(m, x + 1), piles, n, dp));
+        }
+
+        return dp[ind][m] = ans;
+    }
     int stoneGameII(vector<int>& piles) {
-       int length = piles.size();
-        vector<vector<int>>dp(length + 1, vector<int>(length + 1,0));
-        vector<int> sufsum (length + 1, 0);
-        for (int i = length - 1; i >= 0; i--) {
-            sufsum[i] = sufsum[i + 1] + piles[i];
-        }
-        for (int i = 0; i <= length; i++) {
-            dp[i][length] = sufsum[i];
-        }
-        for (int i = length - 1; i >= 0; i--) {
-            for (int j = length - 1; j >= 1; j--) {
-                for (int X = 1; X <= 2 * j && i + X <= length; X++) {
-                    dp[i][j] = max(dp[i][j], sufsum[i] - dp[i + X][max(j, X)]);
-                }
-            }
-        }
-        return dp[0][1]; 
+        int n = piles.size(), sum = 0;
+        for (auto &it : piles) sum += it;
+
+        vector<vector<int>> dp(n, vector<int>(2 * n, -1));
+        int aliceBob = dfs(0, 1, piles, n, dp);
+        return (sum + aliceBob) / 2;
     }
 };
