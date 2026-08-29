308bb59b788a6ca7842192ccad7087a0ceb397b2
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-08-29 07:51:37 +0530

    Time: 350 ms (34.93%), Space: 234.4 MB (23.94%) - LeetHub

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<int> numsSorted(nums);
+        sort(numsSorted.begin(), numsSorted.end());
+
+        int currGroup = 0;
+        unordered_map<int, int> numToGroup;
+        numToGroup.insert(pair<int, int>(numsSorted[0], currGroup));
+
+        unordered_map<int, list<int>> groupToList;
+        groupToList.insert(
+            pair<int, list<int>>(currGroup, list<int>(1, numsSorted[0])));
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
+                currGroup++;
+            }
+
+            numToGroup.insert(pair<int, int>(numsSorted[i], currGroup));
+
+            if (groupToList.find(currGroup) == groupToList.end()) {
+                groupToList[currGroup] = list<int>();
+            }
+            groupToList[currGroup].push_back(numsSorted[i]);
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            int group = numToGroup[num];
+            nums[i] = *groupToList[group].begin();
+            groupToList[group].pop_front();
+        }
+
+        return nums;
+    }
+};
