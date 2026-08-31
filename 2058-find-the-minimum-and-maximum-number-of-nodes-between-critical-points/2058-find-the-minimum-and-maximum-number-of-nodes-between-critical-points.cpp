0f9f9c61ef718ef4748a127b7d4dc0cc7a44a33e
Anjali Dubey <83454075+AKD-01@users.noreply.github.com>
2026-08-31 06:39:22 +0530

    Time: 4 ms (57.39%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,40 +1,41 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-      vector<int>critical;
-        ListNode* cur=head;
-        ListNode* next=head->next;
-        ListNode* prev=NULL;
-        int index=0;
-        while(next!=NULL){
+        vector<int> result = {-1, -1};
 
-            if((prev!=NULL && cur->val<prev->val && cur->val<next->val)||
-            (prev!=NULL && cur->val>prev->val && cur->val>next->val)){
-                critical.push_back(index);
+        int minDistance = INT_MAX;
+
+        ListNode* previousNode = head;
+        ListNode* currentNode = head->next;
+        int currentIndex = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (currentNode->next != nullptr) {
+            if ((currentNode->val < previousNode->val &&
+                 currentNode->val < currentNode->next->val) ||
+                (currentNode->val > previousNode->val &&
+                 currentNode->val > currentNode->next->val)) {
+                if (previousCriticalIndex == 0) {
+                    previousCriticalIndex = currentIndex;
+                    firstCriticalIndex = currentIndex;
+                } else {
+                    minDistance =
+                        min(minDistance, currentIndex - previousCriticalIndex);
+                    previousCriticalIndex = currentIndex;
+                }
             }
-            prev=cur;
-            cur=next;
-            next=next->next;
-            index++;
+
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode->next;
         }
 
-        if(critical.size()<2) return {-1,-1};
-        int maxDist=critical.back()-critical.front();
-        int minDist=INT_MAX;
-
-        for(int i=0;i<critical.size()-1;i++){
-            minDist=min(minDist,critical[i+1]-critical[i]);
+        if (minDistance != INT_MAX) {
+            int maxDistance = previousCriticalIndex - firstCriticalIndex;
+            result = {minDistance, maxDistance};
         }
-        return {minDist,maxDist};  
+
+        return result;
     }
 };
